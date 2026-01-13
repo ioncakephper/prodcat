6bee2dec9877f69a62216b4da4c5d42c8fdb3051
Ion Gireada <ioncakephper@users.noreply.github.com>
2026-01-13 16:42:53 +0200

    feat(blog): Show all posts in blog sidebar

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -58,6 +58,7 @@ const config = {
         },
         blog: {
           showReadingTime: true,
+          blogSidebarCount: 'ALL',
           feedOptions: {
             type: ['rss', 'atom'],
             xslt: true,
